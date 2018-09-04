e2e8bcf097514e96ba2f3a43204304c4ea203254
Veronica Bustabad <vbustabad@gmail.com>
2018-09-04 14:14:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,7 @@ function destructivelyAppendDriver(name) {
 function destructivelyPrependDriver(name) {
   drivers.unshift(name);
 }
+
+function destructivelyRemoveLastDriver(name) {
+  drivers.pop(name);
+}
