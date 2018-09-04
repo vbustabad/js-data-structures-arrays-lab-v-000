dff2f993fa2c2ffeb95939523cdf01d164560fae
Veronica Bustabad <vbustabad@gmail.com>
2018-09-04 14:09:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,12 @@
 
 function drivers () {
   const drivers = ["Milo", "Otis", "Garfield"];
-}  
+}
 
 function destructivelyAppendDriver("Ralph") {
   drivers.push("Ralph");
-}  
+}
+
+function destructivelyPrependDriver("Bob") {
+  drivers.unshift("Bob");
+}
